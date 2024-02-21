963eee02b4b16ad2b9fc9744469cb01288db03b9
uN1CK09 <nicolasdomingos149@gmail.com>
2024-02-20 21:52:58 -0300

    Revert "Revert "Exercícios feitos em aula""
    This reverts commit 6fb06c3f3858c612ba854baf2a3c7aee2010703f.

---
+++ Lt_SeqEx07.java
@@ -0,0 +1,13 @@
+//Receba os valores do comprimento, largura e altura de um paralelepípedo. Calcule e mostre seu volume.
+// volume = (comprimento.largura.altura)
+import javax.swing.JOptionPane;
+public class Lt_SeqEx07 {
+    public static void main (String arg [ ] ) {
+        int comprimento, altura, largura, volume;
+        comprimento = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor do comprimento: "));
+        largura = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor da largura: "));
+        altura = Integer.parseInt(JOptionPane.showInputDialog("Insira o valor da altura: "));
+        volume = (comprimento*largura*altura);
+        JOptionPane.showMessageDialog(null,"O volume do paralelepípedo é: " + volume);
+    }
+}
