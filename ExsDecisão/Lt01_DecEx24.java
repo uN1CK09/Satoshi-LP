e61ccb050d06d78ea24e77a2adb685e31f14cb24
uN1CK09 <150552000+uN1CK09@users.noreply.github.com>
2024-03-05 15:48:39 -0300

    Update Lt01_DecEx24.java

--- ExsDecisão/Lt01_DecEx24.java
+++ ExsDecisão/Lt01_DecEx24.java
@@ -7,7 +7,7 @@ public class Lt01_DecEx24 {
         int num;
         num = Integer.parseInt(JOptionPane.showInputDialog(null, "Insira o número que irá ser verificado"));
 
-        if (num % 2 == 0 && num % 2 == 0) {
+        if (num % 2 == 0 && num % 3 == 0) {
                 JOptionPane.showMessageDialog(null, "O número é divisível por 2 e 3" + "\n" + "Valor: " + num);
         }
         else {
