1389a2ad20dcb8bb81841aec16d3c92086b0e74b
uN1CK09 <nicolasdomingos149@gmail.com>
2024-03-09 18:10:36 -0300

    Fix 42

--- ExsRepetição/Lt01_RepEx42.java
+++ ExsRepetição/Lt01_RepEx42.java
@@ -3,11 +3,13 @@ package ExsRepetição;
 
 public class Lt01_RepEx42 {
     public static void main(String[] args) {
-        double inc, i = 1, calc;
+        double inc, i = 1, soma = 0, calc;
         for (inc = 1; inc <=50; inc ++){
             calc = (inc / i);
             System.out.println(inc + " / " + i + " = " + calc);
+            soma += calc;
             i+=2;
         }
+        System.out.println("Valor da soma de todos os elementos: " + soma);
     }    
 }
