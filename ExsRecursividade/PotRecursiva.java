413082717f6c4ea32a6d63da4d2532bf7365c223
FATEC ZONA LESTE <nicolasdomingos149@gmail.com>
2024-04-11 13:24:54 -0300

    Potência Recursiva

---
+++ ExsRecursividade/PotRecursiva.java
@@ -0,0 +1,25 @@
+package ExsRecursividade;
+import java.util.Scanner;
+public class PotRecursiva {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Insira um valor inteiro: ");
+        
+        int num = scan.nextInt();
+        scan.close();
+        
+        int b = 2;
+        int result = FuncPot(num, b);
+        System.out.println(result);
+    }
+    static int FuncPot(int n, int b){
+        if(b == 1){
+            return n;
+        }
+        else{
+            int pot = n * FuncPot(n, b - 1);
+            return pot;
+        }
+    }    
+}
