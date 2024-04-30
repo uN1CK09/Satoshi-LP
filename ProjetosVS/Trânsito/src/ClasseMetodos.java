754dfcdb054c3983d53e03d9257909f0b9349e09
FATEC ZONA LESTE <nicolasdomingos149@gmail.com>
2024-04-30 17:58:29 -0300

    Refactor Para Registro com escrita e leitura

--- ProjetosVS/Trânsito/src/ClasseMetodos.java
+++ ProjetosVS/Trânsito/src/ClasseMetodos.java
@@ -1,27 +1,40 @@
 import javax.swing.JOptionPane;
-
+import java.io.*;
 public class ClasseMetodos {
-    public Estatisticas[] FuncReg(Estatisticas[] Cidades){
+    public Estatisticas[] FuncReg(Estatisticas[] Cidades) throws IOException {
+        String file = "Registro_Trânsito.txt";
+        BufferedWriter  escrita = new BufferedWriter(new FileWriter(file));
+
         for(int i = 0; i < Cidades.length; i++){
             Cidades[i].CodCidade = Integer.parseInt(JOptionPane.showInputDialog("Insira o código da cidade " + (i+1) + ": "));
+            escrita.write(Integer.toString(Cidades[i].CodCidade)); //Escreve o campo no arquivo
+            escrita.newLine(); //Nova linha
+
             Cidades[i].NomeCidade = JOptionPane.showInputDialog("Insira o nome da cidade " + (i+1) + ": ");
+            escrita.write(Cidades[i].NomeCidade);
+            escrita.newLine();
+
             Cidades[i].QtdAcidentes = Integer.parseInt(JOptionPane.showInputDialog("Insira a quantidade de acidentes da cidade " + (i+1) + ": "));
+            escrita.write(Integer.toString(Cidades[i].QtdAcidentes));
+            escrita.newLine();
         }
+        System.out.println("GRAVAÇÃO REALIZADA");
+        escrita.close(); //Fecha a escrita // FECHAR O OBJETO É *MUITO* IMPORTANTE
         return Cidades;
     }
 
-    public void ConsultaQtd(Estatisticas[] Cidades){
+    public void ConsultaQtd(Estatisticas[] Cidades) throws IOException{
         System.out.println("*Cidades com quantidade de acidentes entre 100 e 500*");
         for(int i = 0; i < Cidades.length; i++){
             if(Cidades[i].QtdAcidentes > 100 && Cidades[i].QtdAcidentes < 500){
                 System.out.println("\nCódigo da cidade: "+Cidades[i].CodCidade + "\nNome da cidade: " + Cidades[i].NomeCidade + "\nQuantidade de acidentes: " + Cidades[i].QtdAcidentes);
             }
         }
-        System.out.println("**********************");
+        System.out.println("\n**********************");
         return;
     }
 
-    public void Maior_Menor(Estatisticas[] Cidades){
+    public void Maior_Menor(Estatisticas[] Cidades)throws IOException{
         int menor = Cidades[0].QtdAcidentes;
         int maior = Cidades[0].QtdAcidentes;
         int indMaior = 0;
@@ -42,11 +55,11 @@ public class ClasseMetodos {
         System.out.println("\n*Menor quantidade de acidentes*" + "\nCódigo da cidade: "+Cidades[indMenor].CodCidade + "\nNome da cidade: " + Cidades[indMenor].NomeCidade + "\nQuantidade de acidentes: " + Cidades[indMenor].QtdAcidentes);
         //Maior Quantidade
         System.out.println("\n*Maior quantidade de acidentes*" + "\nCódigo da cidade: "+Cidades[indMaior].CodCidade + "\nNome da cidade: " + Cidades[indMaior].NomeCidade + "\nQuantidade de acidentes: " + Cidades[indMaior].QtdAcidentes);
-        System.out.println("**********************");
+        System.out.println("\n**********************");
         return;
     }
 
-    public void AcimaMedia(Estatisticas[] Cidades){
+    public void AcimaMedia(Estatisticas[] Cidades)throws IOException{
         int media = 0;
         for(int i = 0; i < Cidades.length; i++){
             media += Cidades[i].QtdAcidentes;
@@ -60,6 +73,24 @@ public class ClasseMetodos {
                 System.out.println("\nCódigo da cidade: "+Cidades[i].CodCidade + "\nNome da cidade: " + Cidades[i].NomeCidade + "\nQuantidade de acidentes: " + Cidades[i].QtdAcidentes + "\n");
             }
         }
-        System.out.println("**********************");
+        System.out.println("\n**********************");
+    }
+
+    public void LerArquivo(Estatisticas[] Cidades) throws IOException{
+        String file = "Registro_Trânsito.txt";
+        BufferedReader leitura = new BufferedReader(new FileReader(file));
+        
+        for(int i = 0; i < Cidades.length; i++){
+            Cidades[i] = new Estatisticas();
+        }
+        
+        for(int i = 0; i < Cidades.length; i++){
+            Cidades[i].CodCidade = Integer.parseInt(leitura.readLine());
+            Cidades[i].NomeCidade = leitura.readLine();
+            Cidades[i].QtdAcidentes = Integer.parseInt(leitura.readLine());
+        }
+
+        leitura.close(); //Fecha a leitura // FECHAR O OBJETO É *MUITO* IMPORTANTE
+        return;
     }
 }
--- ProjetosVS/Trânsito/src/Principal.java
+++ ProjetosVS/Trânsito/src/Principal.java
@@ -1,6 +1,7 @@
+import java.io.IOException;
 import javax.swing.JOptionPane;
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Estatisticas[] Cidades = new Estatisticas[10];
         ClasseMetodos metodos = new ClasseMetodos();
 
@@ -9,7 +10,7 @@ public class Principal {
         }
         int opc = 0;
         while(opc != 9){
-            opc = Integer.parseInt(JOptionPane.showInputDialog("            Acidentes 2020 \n1 - Registro Estatísticas \n2 - Consulta por acidentes \n3 - Maior/Menor número de acidentes \n4 - Acidentes acima da média \n9 - Finalizar"));
+            opc = Integer.parseInt(JOptionPane.showInputDialog("            Acidentes 2020 \n1 - Registro Estatísticas \n2 - Consulta por acidentes \n3 - Maior/Menor número de acidentes \n4 - Acidentes acima da média \n5 - Ler arquivo de registro \n9 - Finalizar"));
             switch (opc) {
                 case 1: //Registro
                     metodos.FuncReg(Cidades);
@@ -22,6 +23,9 @@ public class Principal {
                     break;
                 case 4: //Acidentes acima da média
                     metodos.AcimaMedia(Cidades);
+                    break;
+                case 5://Ler arquivo de registro
+                    metodos.LerArquivo(Cidades);
                     break;
                 case 9:
                     JOptionPane.showMessageDialog(null, "Programa Finalizado");
